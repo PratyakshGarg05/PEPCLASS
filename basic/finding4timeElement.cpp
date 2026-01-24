bed296f7dcaac2b22f57f547941b621b24841cd5
Pratyaksh Garg <gargpratyaksh100@gmail.com>
2026-01-24 10:03:03 +0530

    updated

---
+++ basic/finding4timeElement.cpp
@@ -0,0 +1,20 @@
+#include<unordered_set>
+#include<functional>
+#include<iostream>
+#include<iomanip>
+#include<utility>
+#include<memory>
+#include<string>
+#include<vector>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<list>
+#include<set>
+#include<map>
+using namespace std;
+int main(){
+    int arr[] = {1,1,2,3,1,2,2,3,3,1};
+    
+    return 0;
+}
