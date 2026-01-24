c7543c9d69baec0e3ae09d64f2bb365fb1aee111
Pratyaksh Garg <gargpratyaksh100@gmail.com>
2026-01-24 12:26:36 +0530

    updated pairsum

--- basic/pairsum.cpp
+++ basic/pairsum.cpp
@@ -14,10 +14,20 @@
 #include<map>
 using namespace std;
 int main(){
-    int arr[] = {1,2,3,4,5,6,7,8,9};
-    int target = 6;
+    int size;
+    cout<<"Enter size: ";
+    cin>>size;
+    // int arr[] = {1,2,3,4,5,6,7,8,9};
+    int arr[size];
+    cout<<"Enter sorted array: ";
+    for(int i = 0;i<size;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cout<<"Enter Target: ";
+    cin>>target;
     int start = 0;
-    int end= 8;
+    int end= size-1;
     while(start<end){
         if(arr[start]+arr[end]==target){
             cout<<start<<" "<<end;
