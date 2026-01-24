d5bcf33d0366bcb697e9b02419e9e5197c0e9caa
Pratyaksh Garg <gargpratyaksh100@gmail.com>
2026-01-24 10:40:12 +0530

    updated bubble sort

--- basic/sorting/bubble.cpp
+++ basic/sorting/bubble.cpp
@@ -1,17 +1,4 @@
-#include<unordered_set>
-#include<functional>
 #include<iostream>
-#include<iomanip>
-#include<utility>
-#include<memory>
-#include<string>
-#include<vector>
-#include<deque>
-#include<queue>
-#include<stack>
-#include<list>
-#include<set>
-#include<map>
 using namespace std;
 void bsort(vector<int> &vec){
     int n = vec.size();
