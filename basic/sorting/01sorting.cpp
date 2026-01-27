cd92646eb2ed248cf8dcb7a05aa1012c7871e7e9
Pratyaksh Garg <gargpratyaksh100@gmail.com>
2026-01-27 16:01:56 +0530

    added binary search

---
+++ basic/sorting/01sorting.cpp
@@ -0,0 +1,29 @@
+#include<unordered_set>
+#include<functional>
+#include<iostream>
+#include<iomanip>
+#include<utility>
+#include<memory>
+#include<string>
+#include<vector>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<list>
+#include<set>
+#include<map>
+using namespace std;
+
+int main(){
+    int arr[] = {0,1,0,1,0,0,0,1,1,0,1};
+    int low = 0, high = 11-1;
+    while(low<high){
+        if(arr[low]==1){
+            
+        }
+    }
+    for(auto x: arr){
+        cout<<x<<" ";
+    }
+    return 0;
+}
