380a8c53e326578cd9a9b1dc7016d12d020c3108
Pratyaksh Garg <gargpratyaksh100@gmail.com>
2026-01-31 09:36:43 +0530

    Linked list added

---
+++ linkedlist/structure.cpp
@@ -0,0 +1,28 @@
+#include<unordered_set>
+#include<functional>
+#include<iostream>
+#include<iomanip>
+#include<utility>
+#include<memory>
+#include<string>
+#include<vector>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<list>
+#include<set>
+#include<map>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+int main(){
+    
+    return 0;
+}
